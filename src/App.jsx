8f635b6783703959b50b00707d43720938162264
Nav <navkiran.ghuman@gmail.com>
2017-01-13 16:06:50 -0500

    showing movies that are playing

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,36 @@
 import React, {Component} from 'react';
+import $ from 'jquery';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {movies: []};
+  }
+
+  componentDidMount() {
+    this.NowPlaying();
+  }
+
+  NowPlaying() {
+    return $.getJSON("https://api.themoviedb.org/3/movie/now_playing?api_key=306bd1f9dda87b11475c98f9d47e3862&language=en-US")
+      .then((data) => {
+        this.setState({ movies: data.results });
+        console.log(data.results);
+      });
+  }
+
   render() {
-    return (
-      <h1>Hello React :)</h1>
+    const movie = this.state.movies.map((item) => {
+      return <div>
+          {item.title}
+      </div>
+    });
+
+    return (<div>
+      <h1>Now Playing In Theatres</h1>
+      <h2>{movie}</h2>
+      </div>
     );
   }
 }
